e67e002e4fde83d5d890ef20b888f6a44e5b1292
nairbdryb <nairbdryb@gmail.com>
2023-10-26 20:36:10 -0600

    progress - shows the camera feed

--- mipicam.py
+++ mipicam.py
@@ -9,24 +9,6 @@ def gstreamer_pipeline(
     framerate=60,
     flip_method=0,
 ):
-    # return (
-    #     "nvarguscamerasrc ! "
-    #     "video/x-raw(memory:NVMM), "
-    #     "width=(int)%d, height=(int)%d, "
-    #     "format=(string)NV12, framerate=(fraction)%d/1 ! "
-    #     "nvvidconv flip-method=%d ! "
-    #     "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-    #     "videoconvert ! "
-    #     "video/x-raw, format=(string)BGR ! appsink"
-    #     % (
-    #         capture_width,
-    #         capture_height,
-    #         framerate,
-    #         flip_method,
-    #         display_width,
-    #         display_height,
-    #     )
-    # )
     return (
         "nvarguscamerasrc ! "
         "video/x-raw(memory:NVMM), "
@@ -35,8 +17,7 @@ def gstreamer_pipeline(
         "nvvidconv flip-method=%d ! "
         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
         "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! "
-        "autovideosink"
+        "video/x-raw, format=(string)BGR ! appsink"
         % (
             capture_width,
             capture_height,
@@ -47,5 +28,27 @@ def gstreamer_pipeline(
         )
     )
 
+def get_frames():
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("failed to grab frame")
+            break
+        cv2.imshow("test", frame)
+
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
+
 if __name__ == '__main__':
     cam = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+    if not cam.isOpened():
+        raise SystemExit("Unable to open camera")
+    
+    get_frames()
+
+    cam.release()
+    cv2.destroyAllWindows()
