6cf8dc2b35b16c022d0bc875d23dc44eb57e6fdd
nairbdryb <nairbdryb@gmail.com>
2024-03-25 08:56:37 -0600

    ttarduino

--- talk-to-arduino/send_data.py
+++ talk-to-arduino/send_data.py
@@ -1,12 +1,20 @@
 import serial
 import time
 arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
-def write_read(x):
+
+def write_data(x):
     arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
+
+def read_data():
+    data = arduino.readline().decode("utf-8")
     return data
+
+
 while True:
     num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value) # printing the value
+    write_data(num)
+    for i in range(10):
+        time.sleep(.3)
+        value = read_data() # reading the data from the arduino
+        print(value) # printing the value
+
