3fb58715335e77f10e6eec789a93a97ab4c5d9b7
nairbdryb <nairbdryb@gmail.com>
2024-04-20 11:22:37 -0600

    final changes

--- counter.py
+++ counter.py
@@ -5,7 +5,7 @@ import serial
 from centroidtracker import CentroidTracker
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.6)
-display = jetson.utils.videoOutput("display://0") 
+# display = jetson.utils.videoOutput("display://0") 
 # camera = jetson.utils.videoSource("walking.mp4")      
 camera = jetson.utils.videoSource("csi://0")
 arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
@@ -15,7 +15,7 @@ font = jetson.utils.cudaFont( size=32 )
 ct = CentroidTracker(maxDisappeared=50, maxDistance=50)
 object_id_list = []
 
-SEND_FREQ = 30 # how long in seconds to wait before sending next count
+SEND_FREQ = 300 # how long in seconds to wait before sending next count
 next_time = time.time() + SEND_FREQ # send every 5 minutes
 previous_count = 0
 
@@ -25,7 +25,8 @@ def send_lora(input):
 		arduino.readline() # clear response buffer
 
 
-while display.IsStreaming():
+#while display.IsStreaming():
+while True:
 	img = camera.Capture()
 	rects = []
 	detections = net.Detect(img)
@@ -38,9 +39,9 @@ while display.IsStreaming():
 		if objectId not in object_id_list:
 			object_id_list.append(objectId)
 	
-	font.OverlayText(img, img.width, img.height, "Count: {}".format(len(object_id_list)), 5, 5, (255, 0, 0), (0, 0, 0))
-	display.Render(img)
-	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+	#font.OverlayText(img, img.width, img.height, "Count: {}".format(len(object_id_list)), 5, 5, (255, 0, 0), (0, 0, 0))
+	# display.Render(img)
+	# display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
 
 	if time.time() > next_time:
 		if ((len(object_id_list) - previous_count) > 255):
